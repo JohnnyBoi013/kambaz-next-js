e644e9c41b58ed03fd9d80a8ec2ab4d9dc24aea8
JohnnyBoi013 <jiao.jo@northeastern.edu>
2026-02-08 23:44:08 -0500

    a2 CSS and Bootstrap

--- app/(kambaz)/Navigation.tsx
+++ app/(kambaz)/Navigation.tsx
@@ -1,35 +1,78 @@
+"use client";
+
+import { ListGroup, ListGroupItem } from "react-bootstrap";
 import Link from "next/link";
+
 export default function KambazNavigation() {
   return (
-    <div id="wd-kambaz-navigation">
-      <a href="https://www.northeastern.edu/" id="wd-neu-link" target="_blank">
-        Northeastern
-      </a>
-      <br />
-      <Link href="/account" id="wd-account-link">
+    <ListGroup
+      className="rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2"
+      style={{ width: 120 }}
+      id="wd-kambaz-navigation"
+    >
+      <ListGroupItem
+        className="bg-black border-0 text-center"
+        as="a"
+        target="_blank"
+        href="https://www.northeastern.edu/"
+        id="wd-neu-link"
+      >
+        <img src="/images/NEU.png" width="75px" alt="Northeastern University" />
+      </ListGroupItem>
+
+      <ListGroupItem
+        className="bg-black border-0 text-center text-white"
+        as="a"
+        href="/account"
+        id="wd-account-link"
+      >
         Account
-      </Link>
-      <br />
-      <Link href="/dashboard" id="wd-dashboard-link">
+      </ListGroupItem>
+
+      <ListGroupItem
+        className="bg-black border-0 text-center text-white"
+        as="a"
+        href="/dashboard"
+        id="wd-dashboard-link"
+      >
         Dashboard
-      </Link>
-      <br />
-      <Link href="/courses" id="wd-course-link">
+      </ListGroupItem>
+
+      <ListGroupItem
+        className="bg-black border-0 text-center text-white"
+        as="a"
+        href="/courses"
+        id="wd-course-link"
+      >
         Courses
-      </Link>
-      <br />
-      <Link href="/calendar" id="wd-calendar-link">
+      </ListGroupItem>
+
+      <ListGroupItem
+        className="bg-black border-0 text-center text-white"
+        as="a"
+        href="/calendar"
+        id="wd-calendar-link"
+      >
         Calendar
-      </Link>
-      <br />
-      <Link href="/inbox" id="wd-inbox-link">
+      </ListGroupItem>
+
+      <ListGroupItem
+        className="bg-black border-0 text-center text-white"
+        as="a"
+        href="/inbox"
+        id="wd-inbox-link"
+      >
         Inbox
-      </Link>
-      <br />
-      <Link href="/labs" id="wd-labs-link">
+      </ListGroupItem>
+
+      <ListGroupItem
+        className="bg-black border-0 text-center text-white"
+        as="a"
+        href="/labs"
+        id="wd-labs-link"
+      >
         Labs
-      </Link>
-      <br />
-    </div>
+      </ListGroupItem>
+    </ListGroup>
   );
 }
--- app/labs/TOC.tsx
+++ app/labs/TOC.tsx
@@ -1,40 +1,26 @@
+import { Nav, NavItem, NavLink } from "react-bootstrap";
 import Link from "next/link";
 export default function TOC() {
   return (
-    <ul>
-      <li>
-        <Link href="/labs" id="wd-home-link">
-          Home{" "}
-        </Link>
-      </li>
-      <li>
-        <Link href="/labs/lab1" id="wd-lab1-link">
-          Lab 1{" "}
-        </Link>
-      </li>
-      <li>
-        <Link href="/labs/lab2" id="wd-lab2-link">
-          Lab 2{" "}
-        </Link>
-      </li>
-      <li>
-        <Link href="/labs/lab3" id="wd-lab3-link">
-          Lab 3{" "}
-        </Link>
-      </li>
-      <li>
-        <Link href="/account" id="wd-kambaz-link">
-          Kambaz{" "}
-        </Link>{" "}
-      </li>
-      <li>
-        <Link
-          href="https://github.com/JohnnyBoi013/kambaz-next-js"
-          id="wd-github"
-        >
-          Github{" "}
-        </Link>{" "}
-      </li>
-    </ul>
+    <Nav variant="pills">
+      <NavItem>
+        <NavLink href="/labs">Labs</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="/labs/lab1">Lab 1</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="/labs/lab2">Lab 2</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="/labs/lab3">Lab 3</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="/">Kambaz</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="https://github.com/jannunzi">My GitHub</NavLink>
+      </NavItem>
+    </Nav>
   );
 }
