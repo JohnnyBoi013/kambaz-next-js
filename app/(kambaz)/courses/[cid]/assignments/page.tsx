8028f75d2650a8eed88fe173bc1d03963c5729df
Johnny-Boi-013 <jonjiao13@gmail.com>
2026-02-22 23:53:48 -0500

    test assignments

--- app/(kambaz)/courses/[cid]/assignments/page.tsx
+++ app/(kambaz)/courses/[cid]/assignments/page.tsx
@@ -7,7 +7,7 @@ import { FaCheckCircle } from "react-icons/fa";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 import { Button, FormControl, InputGroup } from "react-bootstrap";
-import assignments from "../../assignments.json";
+import { assignments } from "@/app/(kambaz)/database";
 
 interface Assignment {
   _id: string;
